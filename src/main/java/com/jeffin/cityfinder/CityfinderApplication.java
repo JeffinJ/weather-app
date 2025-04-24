a3def28fcbcf3cab0ecec3680a453cdac07256a8
Jeffin <jeffinj787@gmail.com>
2025-04-24 20:04:49 +0100

    Initial commit

---
+++ src/main/java/com/jeffin/cityfinder/CityfinderApplication.java
@@ -0,0 +1,13 @@
+package com.jeffin.cityfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityfinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityfinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeffin/cityfinder/CityfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffin.cityfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityfinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
